ff0e30849380bdf16eeae53b3fb36b500eecb0ec
Ge Hao <althoughghgh@gmail.com>
2016-11-24 15:33:23 +0800

    优惠券样式完成

--- pages/cart/coupon.js
+++ pages/cart/coupon.js
@@ -1,21 +1,31 @@
+var app = getApp()
+
 Page({
   data: {
-    
+    coupons: []
   },
 
   onShow: function() {
   },
 
   onLoad: function() {
-    this.getCoupon()
+    this.getCoupons()
   },
 
-  getCoupon: function() {
+  getCoupons: function() {
+    // for test
+    // var data = wx.getStorageSync('coupons')
+    // this.setData({coupons: data})
+    var that = this
+    var data = {}
+    data['token'] = app.globalData.token
     wx.request({
-      url: `${getApp().globalData.API_URL}/coupons`,
-      data: {},
+      url: `${app.globalData.API_URL}/coupons`,
+      data: data,
       method: 'GET',
       success: function(res){
+        var data = app.store.sync(res.data)
+        that.setData({coupons: data})
       },
       fail: function() {
       },
