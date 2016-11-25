07f2e16d2714235720f0f6f37dd055d6996c935b
Ge Hao <althoughghgh@gmail.com>
2016-11-25 19:50:59 +0800

    删掉一些没有用的代码节省空间

--- utils/address.js
+++ utils/address.js
@@ -1,35 +1,4 @@
 const app = getApp()
 
-function postAddress (data) {
-  app.request({
-    method: 'POST',
-    url: `${app.globalData.API_URL}/addresses/new`,
-    data: Object.assign({}, data),
-    header: { 'Content-Type': 'application/json' },
-    success (res) {
-      console.log('success')
-      wx.setStorageSync('addrss', res.data)
-      wx.navigateBack()
-    },
-    fail (e) {
-      console.log('error')
-      console.error(e)
-    }
-  })
-}
-
-function getLocation () {
-  wx.getLocation({
-    type: 'gcj02', //返回可以用于 wx.openLocation 的经纬度
-    success: function(res) {
-      var latitude = res.latitude
-      var longitude = res.longitude
-      // TODO
-    }
-  })
-}
-
-
-
 module.exports = {
 }
