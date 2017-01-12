23357976b21fa612f6eb77f16503f661d679653f
Ge Hao <althoughghgh@gmail.com>
2017-01-12 16:09:01 +0800

    staging

--- app.js
+++ app.js
@@ -146,6 +146,6 @@ App({
     userInfo: null,
     currentCustomer: null,
     // API_URL: 'http://localhost:3000',
-    API_URL: 'https://rapi.bayekeji.com'
+    API_URL: 'https://rapi-staging.bayekeji.com'
   }
 })
